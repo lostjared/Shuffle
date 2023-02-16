346366918625eeee8286e083aa49aef9640ba0b8
Jared Bruni <jaredbruni@gmail.com>
2023-02-16 07:15:57 -0800

    Rewrote rust version

--- Shuffle.Rust/src/main.rs
+++ Shuffle.Rust/src/main.rs
@@ -1,50 +1,28 @@
-use rand::thread_rng;
 use shuffle::irs::Irs;
 use shuffle::shuffler::Shuffler;
 use std::env;
-use std::fs;
-use std::io;
 use std::io::prelude::*;
 
-fn shuffle_list(input: &str) {
-    let contents = fs::read_to_string(input).expect("Error reading the file");
-    let mut lines_list: Vec<&str> = contents.lines().collect();
-    let mut rng = thread_rng();
-    let mut irs = Irs::default();
-    irs.shuffle(&mut lines_list, &mut rng).expect("on shuffle");
-    for i in &lines_list {
-        println!("{}", i);
-    }
-}
-
-fn shuffle_input() {
-    let mut v: Vec<String> = Vec::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(l) => {
-                v.push(l);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut v : Vec<String> = Vec::new();
+    if args.len() <= 1 {
+        for i in std::io::stdin().lock().lines() {
+            v.push(i.unwrap());
+        }
+    } else {
+        for i in args.iter().skip(1) {
+            let data = std::fs::read_to_string(&i).unwrap();
+            for z in data.lines() {
+                v.push(z.to_string());
             }
         }
     }
-    let mut rng = thread_rng();
+    let mut rng = rand::thread_rng();
     let mut irs = Irs::default();
     irs.shuffle(&mut v, &mut rng).expect("on shuffle");
     for i in &v {
         println!("{}", i);
     }
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        shuffle_input();
-    } else {
-        for i in args.iter().skip(1) {
-            shuffle_list(i);
-        }
-    }
+    Ok(())
 }
