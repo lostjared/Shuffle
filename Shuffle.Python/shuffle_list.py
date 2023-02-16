ad15075f25cbb08934811f9b64ad18f655fd65bd
Jared Bruni <jaredbruni@gmail.com>
2023-02-16 01:38:16 -0800

    updateD

--- Shuffle.Python/shuffle_list.py
+++ Shuffle.Python/shuffle_list.py
@@ -1,8 +1,18 @@
 import sys
 import random
+
+
 lines = []
-for i in sys.stdin:
-	lines.append(i.rstrip())
+
+if(len(sys.argv) == 1):
+	for i in sys.stdin:
+		lines.append(i.rstrip())
+else:
+	for i in range(1, len(sys.argv)):
+		file = open(sys.argv[i])
+		for z in file:
+			lines.append(z.rstrip())
+
 random.shuffle(lines)
 for i in lines:
 	print(i)
