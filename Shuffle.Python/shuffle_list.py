42da00d278b6b9027e8446e5a2e5a21eebb8cd24
Jared Bruni <jaredbruni@gmail.com>
2023-02-16 01:45:35 -0800

    formatting

--- Shuffle.Python/shuffle_list.py
+++ Shuffle.Python/shuffle_list.py
@@ -1,9 +1,6 @@
 import sys
 import random
-
-
 lines = []
-
 if(len(sys.argv) == 1):
 	for i in sys.stdin:
 		lines.append(i.rstrip())
@@ -12,7 +9,6 @@ else:
 		file = open(sys.argv[i])
 		for z in file:
 			lines.append(z.rstrip())
-
 random.shuffle(lines)
 for i in lines:
 	print(i)
